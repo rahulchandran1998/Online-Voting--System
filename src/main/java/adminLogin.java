c3cdf4c9454d035df00d3bd2b9d0c114978be296
Rahul Chandran <117834388+rahulchandran1998@users.noreply.github.com>
2023-01-03 13:15:32 +0530

    Add files via upload

---
+++ src/main/java/adminLogin.java
@@ -0,0 +1,62 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Servlet implementation class adminLogin
+ */
+public class adminLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+    public adminLogin() {
+        super();
+    }
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        String adminName = request.getParameter("adminName");
+        String password = request.getParameter("password");
+        
+        DatabaseManager db = new DatabaseManager();
+		java.sql.Connection  con = db.getConnection();
+		out.println(adminName);
+		response.sendRedirect("adminWelcome.jsp");
+		
+        
+		try{
+			
+			PreparedStatement preparedStatement = con.prepareStatement("select * from Admin where adminName = '"+adminName+"' and adminPass = '"+password+"'");
+			ResultSet rs=((java.sql.Statement) preparedStatement).executeQuery("select * from Admin where adminName = '"+adminName+"' and adminPass = '"+password+"'");
+			   
+			if(rs.next())
+			   {
+				   HttpSession session=request.getSession();
+				   session.setAttribute("adminname", adminName);
+				   response.sendRedirect("adminWelcome.jsp");
+			     }
+			   else  
+			   {
+				   out.println("WRONG CREDENTIALS");
+			   }
+		   }
+		   catch (Exception e) {
+	             out.println(e.getMessage());
+		   }
+	}
+
+}
---
+++ src/main/webapp/aboutus.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>About us</title>
+<link rel="stylesheet" type="text/css" href="style.css"> 
+</head>
+<%@ include file="navbar.jsp"%> 
+<body>
+<div class="about">
+<p>
+Online voting system" is an online voting technique. In this system people who have citizenship of india and whose age is above 18 years and any sex can give his/her online vote without going to any physical polling station.
+</p>
+<br>
+<h4>About Developer</h4>
+<br>
+<img src="images/rahulimage-ImResizer.jpg" align="right">
+<br>
+Name : Rahul Chandran
+<br>
+Contact number : 8075672547
+<br>
+Email : rahulchandranofcl@gmail.com
+</div>
+</body>
+</html>
